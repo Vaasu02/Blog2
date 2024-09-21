eb9906a2c4f7d2a455a991d85b45409eb03a55dc
Vasu <160459624+Vaasu02@users.noreply.github.com>
2024-09-21 11:13:31 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    // origin: "http://localhost:5173",
+    origin: "https://bloggginghubb.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
