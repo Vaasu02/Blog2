c0848594adbf8bb3098b0a2643ad871a9a0f5422
Vasu <160459624+Vaasu02@users.noreply.github.com>
2024-09-21 11:07:21 +0530

    Update Login.jsx

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3000/auth/login", {
+      const response = await fetch("https://blogg-xs4m.onrender.com/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
